2cc837e8e3e0c9efb4bc3e9d0246ecd14cb261ab
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2020-02-02 08:45:21 +0100

    handle invalid file, error message

--- parser.rb
+++ parser.rb
@@ -1,14 +1,22 @@
 class Parser
+  module MyFile
+    def self.each_line(file_path, &block)
+      File.readlines(file_path).each(&block)
+    end
+  end
+
   def run
     options = get_options
     parse(options[:file_path], $stdout)
-  rescue ArgumentError => e
+  rescue ArgumentError, Errno::ENOENT => e
     puts e.message
     raise e
   end
 
   def parse(file_path, out)
-    out.puts "Hello"
+    MyFile.each_line(file_path) do |line|
+      out.puts line
+    end
   end
 
   private
